306e344391a914568472add3566d85ab0b98a139
陈鹏飞 <913910173@qq.com>
2022-12-22 11:34:33 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="bg-green-500 text-3xl underline font-bold">Hello world!</h1>
+  );
+}
+
+export default App;
